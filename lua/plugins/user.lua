6e106c534eb80367bc6dd1fff78dbc8aa658f13b
Adesi <kopietz123@gmail.com>
2025-12-07 04:13:45 +0100

    adjust dashboard

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -37,7 +37,7 @@ return {
         sections = {
           { section = "header" },
           { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
-          { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+          { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
           { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
           {
             pane = 2,
