d8313f11a04418b26e9438b5d25c275e87079897
Adesi <kopietz123@gmail.com>
2026-03-10 01:34:14 +0100

    feat: add nvim-dap-ui configuration to debug sessions
    Adds nvim-dap-ui support while preserving existing Java and Odin debugger setups.

--- lua/plugins/dap_configurations.lua
+++ lua/plugins/dap_configurations.lua
@@ -1,28 +1,43 @@
 return {
-  "mfussenegger/nvim-dap",
-  config = function()
-    local dap = require "dap"
-    dap.configurations.java = {
-      {
-        type = "java",
-        request = "attach",
-        name = "Attach HytaleServer",
-        hostName = "localhost",
-        port = 5005,
-      },
-    }
-    dap.configurations.odin = {
-      {
-        type = "codelldb",
-        request = "launch",
-        name = "Launch file",
-        program = function()
-          vim.fn.system "odin build ./src/ -debug -o:none"
-          return vim.fn.getcwd() .. "/" .. "src.bin"
-        end,
-        terminal = "integrated",
-        cwd = "${workspaceFolder}",
-      },
-    }
-  end,
+  {
+    "mfussenegger/nvim-dap",
+    config = function()
+      local dap = require "dap"
+      dap.configurations.java = {
+        {
+          type = "java",
+          request = "attach",
+          name = "Attach HytaleServer",
+          hostName = "localhost",
+          port = 5005,
+        },
+      }
+      dap.configurations.odin = {
+        {
+          type = "codelldb",
+          request = "launch",
+          name = "Launch file",
+          program = function()
+            vim.fn.system "odin build ./src/ -debug -o:none"
+            return vim.fn.getcwd() .. "/" .. "src.bin"
+          end,
+          terminal = "integrated",
+          cwd = "${workspaceFolder}",
+        },
+      }
+    end,
+  },
+  {
+    "rcarriga/nvim-dap-ui",
+    config = function(plugin, opts)
+      -- run default AstroNvim nvim-dap-ui configuration function
+      require "astronvim.plugins.configs.nvim-dap-ui"(plugin, opts)
+
+      -- disable dap events that are created
+      local dap = require "dap"
+      dap.listeners.after.event_initialized.dapui_config = nil
+      dap.listeners.before.event_terminated.dapui_config = nil
+      dap.listeners.before.event_exited.dapui_config = nil
+    end,
+  },
 }
